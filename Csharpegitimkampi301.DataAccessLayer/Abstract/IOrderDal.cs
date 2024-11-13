87617c896cab389a17466eb668d195c23c46c191
user <user@DESKTOP-LFJAJU5>
2024-11-13 15:48:42 +0300

    Add project files.

---
+++ Csharpegitimkampi301.DataAccessLayer/Abstract/IOrderDal.cs
@@ -0,0 +1,13 @@
+﻿using Csharpegitimkampi301.Entitylayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Csharpegitimkampi301.DataAccessLayer.Abstract
+{
+    public interface IOrderDal:IGenericDal<Order>
+    {
+    }
+}
